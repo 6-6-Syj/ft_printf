7ab2e189739130536c244c7a828b5e45a47210ac
6-6-Syj <magand.jessy@outlook.fr>
2024-11-22 12:00:27 +0100

    try it

--- ft_idk.c
+++ ft_idk.c
@@ -32,39 +32,6 @@ static int	int_len(long nb)
 	return (count);
 }
 
-int	base_len(char *base)
-{
-	int	i;
-
-	i = 0;
-	while (base[i] != '\0')
-		i++;
-	return (i);
-}
-
-int	check_base(char *base)
-{
-	int	i;
-	int j;
-
-	i = 0;
-	while (base[i] != '\0')
-    {
-		if (base[i] == 43 || base[i] == 45)
-			return (0);
-		j = 0;
-		while (base[j] != '\0')
-		{
-			if (i != j && base[i] == base[j])
-            	return (0);
-    		j++;
-		}
-		i++;
-	}
-	if (i < 2)
-		return (0);
-	return (1);
-}
 int	ft_putnbr(int n)
 {
 	if (n == -2147483648)
@@ -92,8 +59,6 @@ void	ft_putnbr_base(int nbr, char *base)
 	long	nb;
 
 	nb = nbr;
-	if (!check_base(base))
-        return ;
     if (nb < 0)
     {
         nb = nb * -1;
--- ft_printf.c
+++ ft_printf.c
@@ -13,13 +13,48 @@
 #include "printf.h"
 #include <stdarg.h>
 
-int	ft_printf(const char *str, ...)
+ft_printf_args(args, format)
 {
-	// va_list args;
-	// va_start (args, format);
-	// va_end (args);
-	size_t i;
+	if (format[i] == 'c')
+		va_arg(args, int);
+	else if (format[i] == 's')
+		va_arg(args, char);
+	else if (format[i] == 'p')
+		va_arg(args, char);
+	else if (format[i] == 'd')
+		va_arg(args, char);
+	else if (format[i] == 'i')
+		ft_putnbr(va_arg(args, int));
+	else if (format[i] == 'x')
+		va_arg(args, char);
+	else if (format[i] == 'X')
+		va_arg(args, char);
+	else if (format[i] == '%')
+		va_arg(args, char);
+}
 
+int	ft_printf(const char *format, ...)
+{
+	va_list args;
+	size_t	i;
+	size_t	count;
+
+	if (!format)
+		return (-1);
+	va_start (args, format);
 	i = 0;
-	
+	while (format[i])
+	{
+		while (format[i] != '%')
+		{
+			count++;
+			ft_putchar(format[i]);
+			i++;
+		}
+		count += ft_printf_args(args, format);
+		count++;
+		i++;
+	}
+	va_end (args);
+	return (count);
 }
